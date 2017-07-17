33cd028f4ed1176fbc67b2eff8cc4fa6375b9f2f
bcmiller713 <bcmiller713@gmail.com>
2017-07-17 15:47:16 -0700

    cleaning up

--- server.js
+++ server.js
@@ -7,8 +7,6 @@ var port = process.env.port || 3000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(bodyParser.text());
-// app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
 require("./routing/api-routes.js")(app);
 require("./routing/html-routes.js")(app);
